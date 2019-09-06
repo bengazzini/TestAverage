591e4cdcf99d2465b69f529d9a1d90e1ea9ed552
Ben Gazzini <kenpanini@gmail.com>
2019-09-06 13:39:38 -0500

    Behold the beauty of my code.

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -6,7 +6,24 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double score1, score2, score3, score4;
+
+            Console.WriteLine("Enter your first test score.");
+            score1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your second score?");
+            score2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your third score?");
+            score3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your fourth score?");
+            score4 = Convert.ToDouble(Console.ReadLine());
+
+            double result = (score1 + score2 + score3 + score4) / 4;
+
+            Console.WriteLine("The average of your scores is " + result);
+
         }
     }
 }
